9b79f468d2f761be24fc3d6210bf997cf457d520
zuendorf <zuendorf@uni-kassel.de>
2018-08-31 18:41:19 +0200

    starting

---
+++ src/test/java/org/fulib/GenerateModel.java
@@ -0,0 +1,15 @@
+package org.fulib;
+
+import org.junit.Test;
+import org.sdmlib.models.classes.ClassModel;
+
+public class GenerateModel
+{
+   @Test
+   public void testGenerateModel()
+   {
+      ClassModel model = new ClassModel("org.fulib.classmodel");
+
+      model.createClazz("Clazz")
+   }
+}
