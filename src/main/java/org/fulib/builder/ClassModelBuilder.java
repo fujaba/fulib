e7b107bcb8c250bb537042af6692b6a8ead4aecb
Christoph Eickhoff <Christoph.Eickhoff@cs.uni-kassel.de>
2018-09-05 14:46:07 +0200

    added ClassModelBuilder and ClassBuilder

---
+++ src/main/java/org/fulib/builder/ClassModelBuilder.java
@@ -0,0 +1,64 @@
+package org.fulib.builder;
+
+import org.fulib.classmodel.ClassModel;
+
+public class ClassModelBuilder
+{
+   private ClassModel classModel;
+
+
+   /**
+    * @param packagename
+    * @return a class model builder for the given package name and with the default source folder "src/main/java"
+    */
+   public static ClassModelBuilder get(String packagename)
+   {
+      return get(packagename, "src/main/java");
+   }
+
+
+   /**
+    * @param packagename
+    * @param sourceFolder
+    * @return a class model builder for the given package name and source folder 
+    */
+   public static ClassModelBuilder get(String packagename, String sourceFolder)
+   {
+      ClassModelBuilder classModelBuilder = new ClassModelBuilder();
+
+      ClassModel classModel = new ClassModel();
+      classModel.setPackageName(packagename);
+      classModel.setMainJavaDir(sourceFolder);
+
+      classModelBuilder.setClassModel(classModel);
+      return classModelBuilder;
+   }
+
+
+   private void setClassModel(ClassModel classModel)
+   {
+      this.classModel = classModel;
+   }
+
+
+   /**
+    * @return the class model this builder is responsible for 
+    */
+   public ClassModel getClassModel()
+   {
+      return classModel;
+   }
+
+
+   /**
+    * Builds and returns a class builder for the given classname and connects it to the model
+    * @param className
+    * @return new class builder
+    */
+   public ClassBuilder buildClass( String className)
+   {
+      ClassBuilder classBuilder = new ClassBuilder(this.classModel, className);
+      return classBuilder;
+   }
+
+}
--- src/test/java/org/fulib/TestGenerator.java
+++ src/test/java/org/fulib/TestGenerator.java
@@ -1,13 +1,15 @@
 package org.fulib;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.fulib.builder.ClassBuilder;
+import org.fulib.builder.ClassModelBuilder;
 import org.fulib.classmodel.ClassModel;
 import org.fulib.classmodel.Clazz;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class TestGenerator
 {
@@ -36,6 +38,37 @@ public class TestGenerator
       // Generator4CodeGenTests.generate(model);
 
       int returnCode = Javac.compile(model.getPackageSrcFolder() + "/*.java");
+      Assert.assertEquals("compiler return code: ", 0, returnCode);
+
+      // run self test
+
+   }
+   
+   
+   
+   @Test
+   public void testGeneratorWithBuilder()
+   {
+
+      ClassModelBuilder mb = ClassModelBuilder.get("org.fulib.test.studyright","src/test/java");
+      
+      ClassBuilder universitiy = mb.buildClass( "University").buildAttribute("name","String");
+      
+      ClassBuilder studi = mb.buildClass( "Student")
+         .buildAttribute("name","String","\"Karli\"")
+         .buildAttribute("matrNo","long","0");
+
+
+      deleteFile(studi.getClazz());
+
+      Generator.generate(mb.getClassModel());
+      
+      String uniFileName = mb.getClassModel().getPackageSrcFolder() + "/University.java";
+      Assert.assertTrue("University.java exists", Files.exists(Paths.get(uniFileName)));
+
+      // Generator4CodeGenTests.generate(model);
+
+      int returnCode = Javac.compile(mb.getClassModel().getPackageSrcFolder() + "/*.java");
       Assert.assertEquals("compiler return code: ", 0, returnCode);
 
       // run self test
