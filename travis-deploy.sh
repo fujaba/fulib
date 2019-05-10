88d3f4aa0de1a0b3352ea7de3c86be0f0c6fcec1
zuendorf <zuendorf@uni-kassel.de>
2019-05-10 18:39:10 +0200

    travis debug mode

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_d171de1d648a_key -iv $encrypted_d171de1d648a_iv -in my.travis.gpg.enc -out my.travis.gpg -d
-  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
+  ./gradlew uploadArchives --debug -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
 fi
 if [ "$TRAVIS_BRANCH" = 'release' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_d171de1d648a_key -iv $encrypted_d171de1d648a_iv -in my.travis.gpg.enc -out my.travis.gpg -d
-  ./gradlew release -Prelease.useAutomaticVersion=true  -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
+  ./gradlew release --debug -Prelease.useAutomaticVersion=true  -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
 fi
