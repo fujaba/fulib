da4182de5f521107d6eb007c14cb75624f1cb408
Adrian Kunz <clashsoft@hotmail.com>
2019-12-15 15:12:57 +0100

    Correctly handle nulls in Validator

--- src/main/java/org/fulib/util/Validator.java
+++ src/main/java/org/fulib/util/Validator.java
@@ -11,7 +11,7 @@ public class Validator
 {
    public static boolean isQualifiedName(String string)
    {
-      return SourceVersion.isName(string);
+      return string != null && SourceVersion.isName(string);
    }
 
    public static void checkQualifiedName(String string)
@@ -24,14 +24,14 @@ public class Validator
 
    public static boolean isSimpleName(String string)
    {
-      return SourceVersion.isIdentifier(string) && !SourceVersion.isKeyword(string);
+      return string != null && SourceVersion.isIdentifier(string) && !SourceVersion.isKeyword(string);
    }
 
    public static void checkSimpleName(String string)
    {
       if (!isSimpleName(string))
       {
-         throw new IllegalArgumentException("'" + string + "' is not an valid Java identifier");
+         throw new IllegalArgumentException("'" + string + "' is not a valid Java identifier");
       }
    }
 }
