152261f1198a35c7beda9058d26e85fd3372e9d1
Adrian Kunz <clashsoft@hotmail.com>
2019-12-17 19:29:22 +0100

    Clean up TestYamlIdMap and rename to YamlIdMapTest

---
+++ src/test/java/org/fulib/YamlIdMapTest.java
@@ -0,0 +1,84 @@
+package org.fulib;
+
+import org.fulib.builder.ClassBuilder;
+import org.fulib.builder.ClassModelBuilder;
+import org.fulib.builder.Type;
+import org.fulib.classmodel.AssocRole;
+import org.fulib.classmodel.ClassModel;
+import org.fulib.classmodel.Clazz;
+import org.fulib.yaml.YamlIdMap;
+import org.fulib.yaml.YamlObject;
+import org.fulib.yaml.Yamler;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class YamlIdMapTest
+{
+   @Test
+   void testPlainYaml()
+   {
+      String yaml = "" + "joining: abu \n"
+                    + "lastChanges: 2018-03-17T14:48:00.000.abu 2018-03-17T14:38:00.000.bob 2018-03-17T14:18:00.000.xia";
+
+      Yamler yamler = new Yamler();
+
+      LinkedHashMap<String, String> map = yamler.decode(yaml);
+      assertThat(map.get("joining"), equalTo("abu"));
+
+      yaml = "" + "- m: .Map\n" + "  joining: abu\n"
+             + "  lastChanges: 2018-03-17T14:48:00.000.abu 2018-03-17T14:38:00.000.bob 2018-03-17T14:18:00.000.xia";
+
+      YamlIdMap idMap = new YamlIdMap("");
+
+      YamlObject yamlObj = (YamlObject) idMap.decode(yaml);
+
+      assertThat(yamlObj.getMap().get("joining"), equalTo("abu"));
+   }
+
+   @Test
+   void testYamlIdMap()
+   {
+      // create example data
+      ClassModelBuilder mb = Fulib.classModelBuilder("org.fulib.studyright", "tmp/src");
+
+      ClassBuilder university = mb.buildClass("University").buildAttribute("uniName", Type.STRING);
+
+      ClassBuilder student = mb.buildClass("Student").buildAttribute("studentId", Type.STRING);
+
+      university.buildAssociation(student, "students", Type.MANY, "uni", Type.ONE);
+
+      // encode it
+      String packageName = ClassModel.class.getPackage().getName();
+
+      YamlIdMap idMap = new YamlIdMap(packageName);
+
+      String yamlString = idMap.encode(mb.getClassModel());
+
+      assertThat(yamlString, containsString("packageName: \torg.fulib.studyright"));
+      assertThat(yamlString, containsString("name: \tUniversity"));
+      assertThat(yamlString, containsString("name: \tuniName"));
+      assertThat(yamlString, containsString("type: \tString"));
+      assertThat(yamlString, containsString("name: \tstudents"));
+      assertThat(yamlString, containsString("roleType: \t\"java.util.ArrayList<%s>\""));
+      assertThat(yamlString, containsString("name: \tstudentId"));
+
+      // decode it
+      YamlIdMap readMap = new YamlIdMap(packageName);
+
+      Object result = readMap.decode(yamlString);
+
+      ClassModel readModel = (ClassModel) result;
+
+      assertThat(readModel, notNullValue());
+      assertThat(readModel.getClasses().size(), equalTo(2));
+      Clazz clazz = readModel.getClasses().get(0);
+      assertThat(clazz.getAttributes().size(), not(equalTo(0)));
+      assertThat(clazz.getRoles().size(), not(equalTo(0)));
+      AssocRole role = clazz.getRoles().get(0);
+      assertThat(role.getOther(), notNullValue());
+   }
+}
