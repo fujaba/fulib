1f7c1f89a991578b8860b3b6380db9afd4b832fa
Adrian Kunz <clashsoft@hotmail.com>
2019-12-17 19:12:28 +0100

    Clean up ModelEvolutionTest

--- src/test/java/org/fulib/generator/ModelEvolutionTest.java
+++ src/test/java/org/fulib/generator/ModelEvolutionTest.java
@@ -27,10 +27,15 @@ public class ModelEvolutionTest
    @Test
    void testModelEvolution() throws IOException
    {
-      Tools.removeDirAndFiles("tmp");
+      final String targetFolder = "tmp/model-evolution";
+      final String srcFolder = targetFolder + "/src";
+      final String outFolder = targetFolder + "/out";
+      final String packageName = "org.evolve";
+
+      Tools.removeDirAndFiles(targetFolder);
 
       // first simple model
-      ClassModelBuilder mb = Fulib.classModelBuilder("org.evolve", "tmp/src");
+      ClassModelBuilder mb = Fulib.classModelBuilder(packageName, srcFolder);
       ClassBuilder uni = mb.buildClass("University").buildAttribute("uniName", Type.STRING);
       ClassBuilder stud = mb.buildClass("Student").buildAttribute("matNo", Type.STRING)
                             .buildAttribute("startYear", Type.INT);
@@ -39,11 +44,11 @@ public class ModelEvolutionTest
 
       ClassModel firstModel = mb.getClassModel();
 
-      TestGenerator.createPreexistingUniFile("org.evolve", firstModel);
+      TestGenerator.createPreexistingUniFile(packageName, firstModel);
 
       Fulib.generator().generate(firstModel);
 
-      int compileResult = Tools.javac("tmp/out", firstModel.getPackageSrcFolder());
+      int compileResult = Tools.javac(outFolder, firstModel.getPackageSrcFolder());
       assertThat(compileResult, equalTo(0));
       assertThat(Files.exists(Paths.get(firstModel.getPackageSrcFolder() + "/University.java")), is(true));
 
@@ -81,13 +86,13 @@ public class ModelEvolutionTest
       Fulib.generator().generate(firstModel);
       assertThat(logRecordList.size(), not(equalTo(0)));
 
-      compileResult = Tools.javac("tmp/out", firstModel.getPackageSrcFolder());
+      compileResult = Tools.javac(outFolder, firstModel.getPackageSrcFolder());
       assertThat(compileResult, equalTo(0));
 
       // rename a class
       uni.getClazz().setName("Institute");
 
-      // rename an association
+      // TODO rename an association
 
    }
 }
