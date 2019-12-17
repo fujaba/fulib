26fa31a31846ba842755a89ff780002d7713e0eb
Adrian Kunz <clashsoft@hotmail.com>
2019-12-17 18:10:18 +0100

    Clean up test Tools

--- src/test/java/org/fulib/Tools.java
+++ src/test/java/org/fulib/Tools.java
@@ -1,6 +1,5 @@
 package org.fulib;
 
-import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.File;
 import java.io.IOException;
@@ -10,84 +9,75 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Tools {
+public class Tools
+{
+   public static int javac(String outFolder, String sourceFolder)
+   {
+      final String classPath = System.getProperty("java.class.path");
+      return javac(classPath, outFolder, sourceFolder);
+   }
 
-    public static int javac(String classPath, String outFolder, String sourceFolder) {
-        ArrayList<String> args = new ArrayList<>();
+   public static int javac(String classPath, String outFolder, String sourceFolder)
+   {
+      try
+      {
+         Files.createDirectories(Paths.get(outFolder));
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
+      }
 
-        try {
-            Files.createDirectories(Paths.get(outFolder));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+      final File source = new File(sourceFolder);
+      final List<String> args = new ArrayList<>();
 
-        File source = new File(sourceFolder);
+      collectJavaFiles(sourceFolder, args, source);
 
-        collectJavaFiles(sourceFolder, args, source);
+      args.add("-d");
+      args.add(outFolder);
+      args.add("-classpath");
+      args.add(outFolder + File.pathSeparator + classPath);
 
-        args.add("-d");
-        args.add(outFolder);
-        args.add("-classpath");
-        args.add(outFolder + File.pathSeparator + classPath);
+      return ToolProvider.getSystemJavaCompiler().run(null, System.out, System.err, args.toArray(new String[0]));
+   }
 
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+   public static void collectJavaFiles(String sourceFolder, List<String> args, File source)
+   {
+      if (source == null)
+      {
+         return;
+      }
 
-        return compiler.run(null, System.out, System.err, args.toArray(new String[0]));
-    }
+      File[] listFiles = source.listFiles();
+      if (listFiles == null)
+      {
+         return;
+      }
 
+      for (File file : listFiles)
+      {
+         if (file.isDirectory())
+         {
+            collectJavaFiles(sourceFolder + "/" + file.getName(), args, file);
+         }
+         else if (file.getName().endsWith(".java"))
+         {
+            args.add(sourceFolder + "/" + file.getName());
+         }
+      }
+   }
 
+   public static void removeDirAndFiles(String toBeDeletedDir) throws IOException
+   {
+      Path rootPath = Paths.get(toBeDeletedDir);
 
-    public static ArrayList<String> collectJavaFiles(String sourceFolder) {
-        File source = new File(sourceFolder);
-        ArrayList<String> args = new ArrayList<>();
-        collectJavaFiles(sourceFolder, args, source);
+      if (!Files.exists(rootPath))
+      {
+         return;
+      }
 
-        return args;
-    }
-
-
-
-    public static void collectJavaFiles(String sourceFolder, ArrayList<String> args, File source)
-    {
-        if (source == null) {
-            return;
-        }
-
-
-        File[] listFiles = source.listFiles();
-        if (listFiles == null) {
-            return;
-        }
-
-        for (File file : listFiles) {
-            if (file.isDirectory()) {
-                collectJavaFiles(sourceFolder + "/" + file.getName(), args, file);
-            }
-            else if (file.getName().endsWith(".java")) {
-                args.add(sourceFolder + "/" + file.getName());
-            }
-        }
-    }
-
-
-
-    public static int javac(String outFolder, String sourceFolder) {
-        final String classPath = System.getProperty("java.class.path");
-        return javac(classPath, outFolder, sourceFolder);
-    }
-
-    public static void removeDirAndFiles(String toBeDeletedDir) throws IOException {
-        Path rootPath = Paths.get(toBeDeletedDir);
-
-        if (!Files.exists(rootPath)) {
-            return;
-        }
-
-        final List<Path> pathsToDelete = Files.walk(rootPath).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        for (Path path : pathsToDelete) {
-            Files.deleteIfExists(path);
-        }
-    }
+      //noinspection ResultOfMethodCallIgnored
+      Files.walk(rootPath).map(Path::toFile).sorted(Comparator.reverseOrder()).forEach(File::delete);
+   }
 }
