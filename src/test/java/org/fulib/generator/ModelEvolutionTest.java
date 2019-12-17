1e1a7697078c24bfe8af32401ea5c11fae972820
Adrian Kunz <clashsoft@hotmail.com>
2019-12-17 19:10:23 +0100

    Move TestGenerator.testModelEvolution to ModelEvolutionTest

---
+++ src/test/java/org/fulib/generator/ModelEvolutionTest.java
@@ -0,0 +1,93 @@
+package org.fulib.generator;
+
+import org.fulib.Fulib;
+import org.fulib.Generator;
+import org.fulib.TestGenerator;
+import org.fulib.Tools;
+import org.fulib.builder.ClassBuilder;
+import org.fulib.builder.ClassModelBuilder;
+import org.fulib.builder.Type;
+import org.fulib.classmodel.ClassModel;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ModelEvolutionTest
+{
+   @Test
+   void testModelEvolution() throws IOException
+   {
+      Tools.removeDirAndFiles("tmp");
+
+      // first simple model
+      ClassModelBuilder mb = Fulib.classModelBuilder("org.evolve", "tmp/src");
+      ClassBuilder uni = mb.buildClass("University").buildAttribute("uniName", Type.STRING);
+      ClassBuilder stud = mb.buildClass("Student").buildAttribute("matNo", Type.STRING)
+                            .buildAttribute("startYear", Type.INT);
+      uni.buildAssociation(stud, "students", Type.MANY, "uni", Type.ONE);
+      ClassBuilder room = mb.buildClass("Room").buildAttribute("roomNo", Type.STRING);
+
+      ClassModel firstModel = mb.getClassModel();
+
+      TestGenerator.createPreexistingUniFile("org.evolve", firstModel);
+
+      Fulib.generator().generate(firstModel);
+
+      int compileResult = Tools.javac("tmp/out", firstModel.getPackageSrcFolder());
+      assertThat(compileResult, equalTo(0));
+      assertThat(Files.exists(Paths.get(firstModel.getPackageSrcFolder() + "/University.java")), is(true));
+
+      // rename an attribute
+      uni.getClazz().setName("Institute");
+      room.getClazz().setName("LectureHall");
+      stud.getClazz().getAttribute("matNo").setName("studentId");
+      stud.getClazz().getAttribute("startYear").setType(Type.STRING);
+
+      // prepare logger
+      Logger logger = Logger.getLogger(Generator.class.getName());
+      final ArrayList<LogRecord> logRecordList = new ArrayList<>();
+      Handler handler = new Handler()
+      {
+         @Override
+         public void publish(LogRecord record)
+         {
+            logRecordList.add(record);
+         }
+
+         @Override
+         public void flush()
+         {
+         }
+
+         @Override
+         public void close() throws SecurityException
+         {
+         }
+      };
+      logger.setUseParentHandlers(false);
+      logger.addHandler(handler);
+      logger.setLevel(Level.INFO);
+
+      Fulib.generator().generate(firstModel);
+      assertThat(logRecordList.size(), not(equalTo(0)));
+
+      compileResult = Tools.javac("tmp/out", firstModel.getPackageSrcFolder());
+      assertThat(compileResult, equalTo(0));
+
+      // rename a class
+      uni.getClazz().setName("Institute");
+
+      // rename an association
+
+   }
+}
