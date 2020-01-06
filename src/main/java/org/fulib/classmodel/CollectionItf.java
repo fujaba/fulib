b135c938b04d9371abe9692dc4a094ff3d3dd886
Adrian Kunz <clashsoft@hotmail.com>
2020-01-06 16:31:38 +0100

    Add CollectionItf and CollectionType

---
+++ src/main/java/org/fulib/classmodel/CollectionItf.java
@@ -0,0 +1,66 @@
+package org.fulib.classmodel;
+
+import java.util.Collection;
+
+@SuppressWarnings("rawtypes")
+public enum CollectionItf
+{
+   // interfaces corresponding to java.util.Collections.unmodifiable* methods, excluding Maps
+   // must be ordered super -> subtypes
+   Collection(java.util.Collection.class),
+   Set(java.util.Set.class),
+   SortedSet(java.util.SortedSet.class),
+   NavigableSet(java.util.NavigableSet.class),
+   List(java.util.List.class);
+
+   // =============== Fields ===============
+
+   private final Class<? extends Collection> itf;
+
+   // =============== Constructors ===============
+
+   CollectionItf(Class<? extends Collection> itf)
+   {
+      this.itf = itf;
+   }
+
+   // =============== Static Methods ===============
+
+   static CollectionItf deriveFrom(String implClassName)
+   {
+      if (implClassName.contains("List"))
+      {
+         return CollectionItf.List;
+      }
+      else if (implClassName.contains("Set"))
+      {
+         return CollectionItf.Set;
+      }
+      return CollectionItf.Collection;
+   }
+
+   static CollectionItf deriveFrom(Class<? extends Collection> implClass)
+   {
+      final CollectionItf[] values = values();
+      for (int i = values.length - 1; i >= 0; i--)
+      {
+         if (values[i].itf.isAssignableFrom(implClass))
+         {
+            return values[i];
+         }
+      }
+      return Collection;
+   }
+
+   // =============== Properties ===============
+
+   public Class<? extends java.util.Collection> getItfClass()
+   {
+      return this.itf;
+   }
+
+   public String getItfName()
+   {
+      return this.itf.getSimpleName();
+   }
+}
---
+++ src/main/java/org/fulib/classmodel/CollectionType.java
@@ -0,0 +1,114 @@
+package org.fulib.classmodel;
+
+import java.util.Collection;
+
+@SuppressWarnings("rawtypes")
+public class CollectionType
+{
+   // =============== Constants ===============
+
+   public static final CollectionType ArrayList = new CollectionType(CollectionItf.List, "java.util.ArrayList<%s>",
+                                                                     java.util.ArrayList.class);
+
+   public static final CollectionType LinkedHashSet = new CollectionType(CollectionItf.Set,
+                                                                         "java.util.LinkedHashSet<%s>",
+                                                                         java.util.LinkedHashSet.class);
+
+   // =============== Fields ===============
+
+   private final CollectionItf               itf;
+   private final String                      implTemplate;
+   private final Class<? extends Collection> implClass;
+
+   // =============== Constructors ===============
+
+   private CollectionType(CollectionItf itf, String implTemplate, Class<? extends Collection> implClass)
+   {
+      this.itf = itf;
+      this.implTemplate = implTemplate;
+      this.implClass = implClass;
+   }
+
+   // =============== Static Methods ===============
+
+   public static CollectionType of(Class<? extends Collection> implClass)
+   {
+      final CollectionItf itf = CollectionItf.deriveFrom(implClass);
+      final String implTemplate = deriveTemplate(implClass);
+      return new CollectionType(itf, implTemplate, implClass);
+   }
+
+   public static CollectionType of(String implTemplate)
+   {
+      final int genericIndex = implTemplate.indexOf('<');
+      final String className = genericIndex >= 0 ? implTemplate.substring(0, genericIndex) : implTemplate;
+
+      try
+      {
+         final Class<?> implClass = Class.forName(className);
+
+         if (!(Collection.class.isAssignableFrom(implClass)))
+         {
+            throw new IllegalArgumentException("class '" + className + "' is not a sub-type of java.util.Collection");
+         }
+
+         final CollectionItf itf = CollectionItf.deriveFrom((Class<? extends Collection>) implClass);
+         return new CollectionType(itf, implTemplate, (Class<? extends Collection>) implClass);
+      }
+      catch (ClassNotFoundException e)
+      {
+         final CollectionItf itf = CollectionItf.deriveFrom(className);
+         return new CollectionType(itf, implTemplate, null);
+      }
+   }
+
+   private static String deriveTemplate(Class<? extends Collection> implClass)
+   {
+      if (!Collection.class.isAssignableFrom(implClass))
+      {
+         throw new IllegalArgumentException(
+            "class '" + implClass.getName() + "' is not a sub-type of java.util.Collection");
+      }
+
+      final String roleType = implClass.getName();
+      final int typeParamCount = implClass.getTypeParameters().length;
+      switch (typeParamCount)
+      {
+      case 0:
+         return roleType;
+      case 1:
+         return roleType + "<%s>";
+      default:
+         throw new IllegalArgumentException(
+            "class '" + implClass.getName() + "' has too many type parameters (" + typeParamCount
+            + "), only 0 or 1 are supported");
+      }
+   }
+
+   // =============== Properties ===============
+
+   public CollectionItf getItf()
+   {
+      return this.itf;
+   }
+
+   public Class<? extends Collection> getItfClass()
+   {
+      return this.itf.getItfClass();
+   }
+
+   public String getItfName()
+   {
+      return this.itf.getItfName();
+   }
+
+   public String getImplTemplate()
+   {
+      return this.implTemplate;
+   }
+
+   public Class<? extends Collection> getImplClass()
+   {
+      return this.implClass;
+   }
+}
