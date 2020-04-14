d231c79956510054fd5aa248a6f40f0307fcde34
Adrian Kunz <clashsoft@hotmail.com>
2020-04-14 15:04:41 +0200

    Fix ViewFragments

--- src/test/java/org/fulib/ViewFragments.java
+++ src/test/java/org/fulib/ViewFragments.java
@@ -1,24 +1,20 @@
 package org.fulib;
 
-import org.fulib.classmodel.CodeFragment;
 import org.fulib.classmodel.FileFragmentMap;
 import org.fulib.parser.FragmentMapBuilder;
 
-import java.io.IOException;
-
 public class ViewFragments
 {
-   public static void main(String[] args) throws IOException
+   public static void main(String[] args)
    {
       final FileFragmentMap map = FragmentMapBuilder.parse(args[0]);
 
       System.out.println("/////////////// " + map.getFileName() + " ///////////////");
 
-      for (final CodeFragment fragment : map.getFragments())
-      {
+      map.codeFragments().forEach(fragment -> {
          System.out.println("// --------------- " + fragment.getKey() + " ---------------");
          System.out.print(fragment.getText());
-      }
+      });
 
       System.out.println("/////////////// EOF ///////////////");
    }
