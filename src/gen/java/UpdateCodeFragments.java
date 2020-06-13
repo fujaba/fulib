f9d6d2adb1e70efa2aad3ce5d13b58d94487b20b
Adrian Kunz <clashsoft@hotmail.com>
2020-06-13 18:27:35 +0200

    Add UpdateCodeFragments

---
+++ src/gen/java/UpdateCodeFragments.java
@@ -0,0 +1,13 @@
+import org.fulib.FulibTools;
+import org.fulib.tools.CodeFragments;
+
+public class UpdateCodeFragments
+{
+   // TODO this would be better as a test that automatically runs
+   public static void main(String[] args)
+   {
+      final CodeFragments fragments = FulibTools.codeFragments();
+      fragments.load("test/");
+      fragments.write("README.md");
+   }
+}
