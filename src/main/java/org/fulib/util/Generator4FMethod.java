3b26c7b3223c59cf68b35030e06af75765e967b5
Adrian Kunz <clashsoft@hotmail.com>
2020-06-17 23:08:11 +0200

    Remove commented code from Generator4FMethod

--- src/main/java/org/fulib/util/Generator4FMethod.java
+++ src/main/java/org/fulib/util/Generator4FMethod.java
@@ -10,41 +10,11 @@ import org.fulib.classmodel.FMethod;
 @Deprecated
 public class Generator4FMethod
 {
+   /**
+    * @deprecated since 1.2; does nothing
+    */
+   @Deprecated
    public void generate(FMethod method)
    {
-      //      String packageDir = method.getPackageName().replaceAll("\\.", "/");
-      //      String srcDir = method.getJavaSrcDir() + "/" + packageDir;
-      //      String classFileName = srcDir + "/" + method.getClassName() + ".java";
-      //      FileFragmentMap fragmentMap = Parser.parse(classFileName);
-      //
-      //      String signature = method.getSignature();
-//      String methodBody = method.getMethodBody();
-//      if (methodBody == null) {
-//         methodBody = "      // hello world\n";
-//      }
-//      String newText = "   " + method.getDeclaration() +
-//            "{ \n" +
-//            methodBody +
-//            "   }";
-//
-//      fragmentMap.add(signature, newText, 2);
-//
-//      fragmentMap.add(Parser.CLASS_END, "", 2, true);
-//      fragmentMap.add(Parser.CLASS_END, "}", 1, false);
-//
-//      if (fragmentMap.classBodyIsEmpty(fragmentMap)) {
-//         Path path = Paths.get(classFileName);
-//         try {
-//            Files.deleteIfExists(path);
-//            Logger.getLogger(Generator.class.getName())
-//                  .info("\n   deleting empty file " + classFileName);
-//         } catch (IOException e) {
-//            e.printStackTrace();
-//         }
-//      } else {
-//         fragmentMap.writeFile();
-//      }
-//
-//      System.out.println();
    }
 }
