cfac7b700149a5b24c2bb36986ed4d2184088042
Adrian Kunz <clashsoft@hotmail.com>
2020-06-17 22:59:04 +0200

    Clean up Type

--- src/main/java/org/fulib/builder/Type.java
+++ src/main/java/org/fulib/builder/Type.java
@@ -5,20 +5,20 @@ public class Type
    // value types
    // for attributes
    public static final String BOOLEAN = "boolean";
-   public static final String INT     = "int";
-   public static final String LONG    = "long";
-   public static final String FLOAT   = "float";
-   public static final String DOUBLE  = "double";
-   public static final String STRING  = "String";
+   public static final String INT = "int";
+   public static final String LONG = "long";
+   public static final String FLOAT = "float";
+   public static final String DOUBLE = "double";
+   public static final String STRING = "String";
 
    // cardinalities
    // for associations
-   public static final int ONE  = 1;
+   public static final int ONE = 1;
    public static final int MANY = 42;
 
    // property styles
    // for attributes and associations
-   public static final String POJO    = "POJO";
-   public static final String BEAN    = "Bean";
+   public static final String POJO = "POJO";
+   public static final String BEAN = "Bean";
    public static final String JAVA_FX = "JavaFX";
 }
