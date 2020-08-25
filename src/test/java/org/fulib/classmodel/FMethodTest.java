17afe61f4ed60803d0b2eabfe64d3fb0d932f09f
Adrian Kunz <clashsoft@hotmail.com>
2020-08-26 01:10:25 +0200

    test: Check FMethod.getSignature in FMethodTest

--- src/test/java/org/fulib/classmodel/FMethodTest.java
+++ src/test/java/org/fulib/classmodel/FMethodTest.java
@@ -11,15 +11,18 @@ class FMethodTest
    @Test
    void setDeclaration()
    {
-      final FMethod simple = new FMethod();
+      final Clazz clazz = new Clazz().setName("Foo");
+
+      final FMethod simple = new FMethod().setClazz(clazz);
       simple.setDeclaration("void foo()");
       assertThat(simple.getModifiers(), equalTo(""));
       assertThat(simple.getReturnType(), equalTo("void"));
       assertThat(simple.getAnnotations(), emptyString());
       assertThat(simple.getName(), equalTo("foo"));
       assertThat(simple.getParams(), anEmptyMap());
+      assertThat(simple.getSignature(), equalTo("class/Foo/method/foo()"));
 
-      final FMethod oneParam = new FMethod();
+      final FMethod oneParam = new FMethod().setClazz(clazz);
       oneParam.setDeclaration("@NonNull private String bar(String par1)");
       assertThat(oneParam.getModifiers(), equalTo("private"));
       assertThat(oneParam.getReturnType(), equalTo("String"));
@@ -27,8 +30,9 @@ class FMethodTest
       assertThat(oneParam.getName(), equalTo("bar"));
       assertThat(oneParam.getParams(), aMapWithSize(1));
       assertThat(oneParam.getParams(), hasEntry("par1", "String"));
+      assertThat(oneParam.getSignature(), equalTo("class/Foo/method/bar(String)"));
 
-      final FMethod twoParams = new FMethod();
+      final FMethod twoParams = new FMethod().setClazz(clazz);
       twoParams.setDeclaration("@Override @NonNull protected synchronized String baz(String par1, int par2)");
       assertThat(twoParams.getModifiers(), equalTo("protected synchronized"));
       assertThat(twoParams.getReturnType(), equalTo("String"));
@@ -37,16 +41,18 @@ class FMethodTest
       assertThat(twoParams.getParams(), aMapWithSize(2));
       assertThat(twoParams.getParams(), hasEntry("par1", "String"));
       assertThat(twoParams.getParams(), hasEntry("par2", "int"));
+      assertThat(twoParams.getSignature(), equalTo("class/Foo/method/baz(String,int)"));
 
-      final FMethod varargs = new FMethod();
+      final FMethod varargs = new FMethod().setClazz(clazz);
       varargs.setDeclaration("public void varargs(String... args)");
       assertThat(varargs.getModifiers(), equalTo("public"));
       assertThat(varargs.getReturnType(), equalTo("void"));
       assertThat(varargs.getName(), equalTo("varargs"));
       assertThat(varargs.getParams(), aMapWithSize(1));
       assertThat(varargs.getParams(), hasEntry("args", "String..."));
+      assertThat(varargs.getSignature(), equalTo("class/Foo/method/varargs(String...)"));
 
-      final FMethod parametricTypes = new FMethod();
+      final FMethod parametricTypes = new FMethod().setClazz(clazz);
       parametricTypes.setDeclaration(
          "public Map<String, Integer> parametricTypes(List<Integer> ints, Map<Integer, Map<Integer, String>> matrix)");
       assertThat(parametricTypes.getModifiers(), equalTo("public"));
@@ -55,27 +61,34 @@ class FMethodTest
       assertThat(parametricTypes.getParams(), aMapWithSize(2));
       assertThat(parametricTypes.getParams(), hasEntry("ints", "List<Integer>"));
       assertThat(parametricTypes.getParams(), hasEntry("matrix", "Map<Integer, Map<Integer, String>>"));
+      assertThat(parametricTypes.getSignature(),
+                 equalTo("class/Foo/method/parametricTypes(List<Integer>,Map<Integer,Map<Integer,String>>)"));
    }
 
    @Test
    void setDeclaration_cStyleArrays()
    {
-      final FMethod cStyleArrays = new FMethod();
+      final Clazz clazz = new Clazz().setName("Foo");
+
+      final FMethod cStyleArrays = new FMethod().setClazz(clazz);
       cStyleArrays.setDeclaration("String cStyleArrays(String args[])[]");
       assertThat(cStyleArrays.getReturnType(), equalTo("String[]"));
       assertThat(cStyleArrays.getParams(), aMapWithSize(1));
       assertThat(cStyleArrays.getParams(), hasEntry("args", "String[]"));
+      assertThat(cStyleArrays.getSignature(), equalTo("class/Foo/method/cStyleArrays(String[])"));
 
-      final FMethod cStyleMultiArrays = new FMethod();
+      final FMethod cStyleMultiArrays = new FMethod().setClazz(clazz);
       cStyleMultiArrays.setDeclaration("String cStyleMultiArrays(String args[][])[][]");
       assertThat(cStyleMultiArrays.getReturnType(), equalTo("String[][]"));
       assertThat(cStyleMultiArrays.getParams(), aMapWithSize(1));
       assertThat(cStyleMultiArrays.getParams(), hasEntry("args", "String[][]"));
+      assertThat(cStyleMultiArrays.getSignature(), equalTo("class/Foo/method/cStyleMultiArrays(String[][])"));
 
-      final FMethod cStyleMixedArrays = new FMethod();
+      final FMethod cStyleMixedArrays = new FMethod().setClazz(clazz);
       cStyleMixedArrays.setDeclaration("String[] cStyleMixedArrays(String[] args[])[]");
       assertThat(cStyleMixedArrays.getReturnType(), equalTo("String[][]"));
       assertThat(cStyleMixedArrays.getParams(), aMapWithSize(1));
       assertThat(cStyleMixedArrays.getParams(), hasEntry("args", "String[][]"));
+      assertThat(cStyleMixedArrays.getSignature(), equalTo("class/Foo/method/cStyleMixedArrays(String[][])"));
    }
 }
