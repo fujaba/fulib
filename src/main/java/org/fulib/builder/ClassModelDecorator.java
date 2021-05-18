8a321bfd948d6ac493852221c7e94f81129d125d
Adrian Kunz <clashsoft@hotmail.com>
2021-05-18 16:51:49 +0200

    docs: Add JavaDocs for ClassModelDecorator and ClassModelDecorators

--- src/main/java/org/fulib/builder/ClassModelDecorator.java
+++ src/main/java/org/fulib/builder/ClassModelDecorator.java
@@ -1,6 +1,17 @@
 package org.fulib.builder;
 
+/**
+ * ClassModelDecorator specifies hooks for various steps of the fulib lifecycle.
+ *
+ * @since 1.6
+ */
 public interface ClassModelDecorator
 {
+   /**
+    * Hook for modifying the class meta model.
+    *
+    * @param m
+    *    the class model manager
+    */
    void decorate(ClassModelManager m);
 }
--- src/main/java/org/fulib/builder/ClassModelDecorators.java
+++ src/main/java/org/fulib/builder/ClassModelDecorators.java
@@ -5,9 +5,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for specifying which classes in a package are {@link ClassModelDecorator}s.
+ *
+ * @since 1.6
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PACKAGE)
 public @interface ClassModelDecorators
 {
+   /**
+    * @return the classes in this package that implement {@link ClassModelDecorator}
+    */
    Class<? extends ClassModelDecorator>[] value();
 }
