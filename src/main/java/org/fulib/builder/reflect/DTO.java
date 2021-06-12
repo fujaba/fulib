97ac422dbd6bf6211e5011aa46242b19c41ef64e
Adrian Kunz <clashsoft@hotmail.com>
2021-06-12 14:00:35 +0200

    docs: Add DTO JavaDocs

--- src/main/java/org/fulib/builder/reflect/DTO.java
+++ src/main/java/org/fulib/builder/reflect/DTO.java
@@ -5,10 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Allows creating Data Transfer Object (DTO) classes by copying attributes from a model class.
+ * Associations are automatically converted to String attributes meant for holding the ID of the link target.
+ *
+ * @since 1.6
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DTO
 {
+   /**
+    * @return the model class to copy attributes from
+    */
    Class<?> model();
 
    /**
